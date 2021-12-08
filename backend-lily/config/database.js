35ec5b81ce56ee6782360cde8f211332ceb88d7d
Samuel <samuelgaliere.pro@gmail.com>
2021-12-08 18:40:13 +0100

    fixed database

--- backend-lily/config/database.js
+++ backend-lily/config/database.js
@@ -4,8 +4,13 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "172.17.0.2"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "lily-brdn-db"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "Actimel164."),
+        schema: env("DATABASE_SCHEMA", "public"),
       },
       options: {
         useNullAsDefault: true,
