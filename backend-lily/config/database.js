d7827e6e85bd6f3d22e94fa4932646358af6983f
Samuel GALIERE <samuelgaliere.pro@gmail.com>
2021-12-09 16:43:49 +0100

    fixed db connection problem

--- backend-lily/config/database.js
+++ backend-lily/config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "lily-brdn-db"),
         username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "Actimel164."),
+        password: env("DATABASE_PASSWORD", "Actimel164"),
         schema: env("DATABASE_SCHEMA", "public"),
       },
       options: {
