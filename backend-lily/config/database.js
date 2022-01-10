3ae8db8c5fb7c64b3d0cf0fd8737cb5479e53172
Samuel <samuelgaliere.pro@gmail.com>
2022-01-11 00:13:27 +0100

    testing some config changes

--- backend-lily/config/database.js
+++ backend-lily/config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "172.17.0.3"),
+        host: env("DATABASE_HOST", "172.17.0.1"),
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "lily-brdn-db"),
         username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "Actimel164"),
+        password: env("DATABASE_PASSWORD", "Actimel164."),
         schema: env("DATABASE_SCHEMA", "public"),
       },
       options: {
